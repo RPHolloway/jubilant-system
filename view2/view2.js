f0d89036ae41067bbf198a95bae4eb19f2bfc741
rholloway <rick@tri-ware.com>
2017-06-04 13:05:36 -0400

    Panning seems to work with bar charts not line charts....

--- view2/view2.js
+++ view2/view2.js
@@ -19,13 +19,13 @@ angular.module('myApp.view2', ['ngRoute'])
   ctx.style.height = Size[1] - 100 + "px";
 
   var linechart = new chart(ctx, {
-    type: 'line',
+    type: 'bar',
     responsive: true,
     data: {
-      labels: [0], //[0, 1, 2, 3, 4, 5],
+      labels: [0],
       datasets: [{
         label: 'series 1',
-        data: [0], //[12, 19, 3, 5, 2, 3],
+        data: [0],
         borderWidth: 1
       }]
     },
@@ -35,25 +35,24 @@ angular.module('myApp.view2', ['ngRoute'])
       },
       pan: {
         enabled: true,
-        mode: 'y'
+        mode: 'x'
       },
       zoom: {
         enabled: true,
-        mode: 'y',
-        rangeMin: {
-          x: null,
-          y: -1.5
-        },
-        rangeMax: {
-          x: null,
-          y: 1.5
-        }
+        sensitivity: .1,
+        mode: 'x',
       },
       scales: {
         yAxes: [{
           ticks: {
             beginAtZero: true
           }
+        }],
+        xAxes: [{
+           ticks: {
+             autoSkip: true,
+             maxTicksLimit: 25
+           }
         }]
       }
     }
@@ -65,7 +64,7 @@ angular.module('myApp.view2', ['ngRoute'])
   var amplitude = 1;
   var t = 0;
   function spoofData() {
-      linechart.data.labels.push(t);
+      linechart.data.labels.push(t.toFixed(2));
       linechart.data.datasets[0].data.push(Math.sin(frequency*t)*amplitude);
       t += .01;
       linechart.update();
